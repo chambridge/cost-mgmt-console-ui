bd382f2a8928a9db295957b30001ad5ef3a7a73b
Chris Hambridge <chambrid@redhat.com>
2025-02-17 14:47:57 -0500

    rebuild

--- src/components/ExamplePage.tsx
+++ src/components/ExamplePage.tsx
@@ -50,34 +50,11 @@ export default function ExamplePage() {
         <PageSection>
           <Title headingLevel="h1">{t('Projects')}</Title>
         </PageSection>
-        {/* <PageSection variant="light">
-          <TextContent>
-            <Text component="p">
-              <span className="cost-mgmt-ui-console-plugin__nice">
-                <CheckCircleIcon /> {t('Success!')}
-              </span>{' '}
-              {t('Your plugin is working.')}
-            </Text>
-            <Text component="p">
-              {t(
-                'This is a custom page contributed by the console plugin template. The extension that adds the page is declared in console-extensions.json in the project root along with the corresponding nav item. Update console-extensions.json to change or add extensions. Code references in console-extensions.json must have a corresponding property',
-              )}
-              <code>{t('exposedModules')}</code>{' '}
-              {t('in package.json mapping the reference to the module.')}
-            </Text>
-            <Text component="p">
-              {t('After cloning this project, replace references to')}{' '}
-              <code>{t('console-template-plugin')}</code>{' '}
-              {t('and other plugin metadata in package.json with values for your plugin.')}
-            </Text>
-          </TextContent>
-        </PageSection> */}
-
         <PageSection>
           <Table variant="compact">
             <Thead>
               <Tr>
-                <Th> Project</Th>
+                <Th>Project</Th>
                 <Th>Cost</Th>
               </Tr>
             </Thead>
