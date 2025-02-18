6ae5b0db555cbea0cd38e2b7415b8761db925b4f
Chris Hambridge <chambrid@redhat.com>
2025-02-18 11:05:12 -0500

    fix grabbing cost

--- src/components/ExamplePage.tsx
+++ src/components/ExamplePage.tsx
@@ -30,7 +30,7 @@ async function fetchProjects() {
         console.log("data - projects = " + jsonData["data"][0]["projects"]);
         for (var project of jsonData["data"][0]["projects"]) {
           console.log(project);
-          var projData = {"name": project["project"], "cost": project["values"][0]["cost"]["value"]};
+          var projData = {"name": project["project"], "cost": project["values"][0]["cost"]["total"]["value"]};
           projects.push(projData);
         }
       } else {
